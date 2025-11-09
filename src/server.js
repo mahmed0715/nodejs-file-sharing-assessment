a649260ceab76c19db8cc72ba8cb5865e8fac738
Mustak Ahmed <mahmed0715@gmail.com>
2025-11-09 13:29:58 +0600

    Udpate port host for deployed version

--- src/server.js
+++ src/server.js
@@ -5,13 +5,12 @@ const app = require('./app');
 const fs = require('fs');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
 
 // Ensure storage folder exists
 const storageFolder = process.env.FOLDER || path.resolve(__dirname, '..', 'storage_root');
 if (!fs.existsSync(storageFolder)) fs.mkdirSync(storageFolder, { recursive: true });
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`File Sharing API listening on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
